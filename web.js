2f8c090862c2a99c9466bed727be9dc61f4a5f8a
Nikunj Handa <nikunj.sg@gmail.com>
2015-02-20 13:45:16 +0000

    temporarily disable https redirect

--- web.js
+++ web.js
@@ -6,11 +6,12 @@ var logger = require('morgan');
 
 var nodeApp = express();
 
-nodeApp.use(function(req, res, next) {
-  var schema = req.headers['x-forwarded-proto'];
-  if (schema === 'https') { return next();}
-  res.redirect('https://' + req.headers.host + req.url);
-});
+// nodeApp.use(function(req, res, next) {
+//   var schema = req.headers['x-forwarded-proto'];
+//   if (schema === 'https') { return next();}
+//   res.redirect('https://' + req.headers.host + req.url);
+// });
+
 nodeApp.use(logger('dev'));
 nodeApp.use(gzippo.staticGzip('' + __dirname + '/dist'));
 
