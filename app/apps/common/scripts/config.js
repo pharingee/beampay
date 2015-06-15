ae55699b63d50c75cace280e8e7aff88ceff2206
Gerald Pharin <gerald@pharin.me>
2015-06-15 14:43:03 +0000

    fix(scss): fix fontawesome import in main.scss

--- app/apps/common/scripts/config.js
+++ app/apps/common/scripts/config.js
@@ -2,6 +2,6 @@
 
 angular
   .module('app.config', [])
-  .constant('API_SERVER', 'http://localhost:8888/api/v1/')
+  .constant('API_SERVER', 'http://localhost:8000/api/v1/')
   .constant('FACEBOOK_CLIENT_ID', 1562773807273610);
   // .constant('API_SERVER', 'https://beampay-dev.herokuapp.com/api/v1/');
