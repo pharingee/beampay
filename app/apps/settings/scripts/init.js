edda875fdc55f5d61ceea2772c42a21c48657dc2
Gerald Pharin <gerald@pharin.me>
2015-06-23 10:37:45 +0000

    fix(onboarding): Unset onboarding as a sub state of settings

--- app/apps/settings/scripts/init.js
+++ app/apps/settings/scripts/init.js
@@ -6,20 +6,20 @@ angular
     var tempPrefix ='apps/settings/views/';
 
     $stateProvider
-      .state('settings.onboard', {
-        url: '/settings/onboard',
+      .state('onboard', {
+        url: '^/settings/onboard',
         abstract: 'true',
         template: '<ui-view/>'
       })
-      .state('settings.onboard.name', {
-        url: '/settings/onboard/name',
+      .state('onboard.name', {
+        url: '^/settings/onboard/name',
         templateUrl: tempPrefix + 'onboardName.html',
         data: {
           pageTitle: 'Onboarding - Name'
         }
       })
-      .state('settings.onboard.address', {
-        url: '/settings/onboard/address',
+      .state('onboard.address', {
+        url: '^/settings/onboard/address',
         templateUrl: tempPrefix + 'onboardAddress.html',
         data: {
           pageTitle: 'Onboarding - Address'
