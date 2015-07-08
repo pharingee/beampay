931fa9ea22bf5856057f7cd87600cf3298cf24bb
Gerald Pharin <gerald@pharin.me>
2015-07-08 21:17:48 +0000

    heroku stage commiy

--- app/apps/common/scripts/config.js
+++ app/apps/common/scripts/config.js
@@ -6,7 +6,7 @@
 
 .constant('API_SERVER', 'https://api-dev.beampay.co/api/v1/')
 
-.constant('FACEBOOK_CLIENT_ID', 1562773807273610)
+.constant('FACEBOOK_CLIENT_ID', 1598968723654118)
 
 .constant('STRIPE_KEY', 'pk_test_zYX7SkWrbMZCszrPcoDPu4iI')
 
