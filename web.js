c7bc212efd7b9a9b395128955e3547d65e4bccf7
fbenke <falk@meltwater.org>
2015-07-11 21:25:12 +0000

    share email call goes to backend now

--- web.js
+++ web.js
@@ -38,37 +38,4 @@ app.use(gzippo.staticGzip('' + __dirname + '/dist', {
   clientMaxAge: 1000
 }));
 
-// Share Email route
-app.post('/email', function (req, res) {
-
-  var emailBody = 'Hi '+ req.body.toname + ',\n\n' +
-    'I thought you\'d like to know about Beam  - a way for Ghanaians abroad ' +
-    'to care for loved ones back home by paying directly for school ' +
-    'fees, hospital bills, phone airtime credit, and much more.\n\nFind out ' +
-    'how: http://beampay.co';
-
-  request.post('https://api.sendgrid.com/api/mail.send.json',{
-      form: {
-        'api_user': process.env.SENDGRID_USER,
-        'api_key': process.env.SENDGRID_PASSWORD,
-        subject: 'Take a look at Beam',
-        text: emailBody,
-        from: 'hello@beampay.co',
-
-        to: req.body.to,
-        toname: req.body.toname,
-        fromname: req.body.fromname,
-        replyto: req.body.replyto
-      }
-    },
-    function (err, response, body) {
-      if (err) {
-        res.status(500).send(body);
-      } else {
-        res.status(response.statusCode).send(body);
-      }
-    }
-  );
-});
-
 app.listen(process.env.PORT || 9000);
