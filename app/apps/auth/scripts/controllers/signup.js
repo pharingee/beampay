6094d62e3bac846c24c40dfa4f6970bff1ed5f36
Gerald Pharin <gerald@pharin.me>
2015-07-17 23:03:03 +0000

    fix(auth): service icons to onboarding

--- app/apps/auth/scripts/controllers/signup.js
+++ app/apps/auth/scripts/controllers/signup.js
@@ -55,11 +55,17 @@ angular
       }).then(function (req) {
         Auth.persist(req.data.id, req.data.token, req.data.complete);
         Auth.saveName(req.data.firstName, req.data.lastName);
-        if ($stateParams.next) {
-          $location.path($state.href($stateParams.next).slice(2));
+
+        if (!req.data.complete) {
+          $location.path($state.href('onboard.name').slice(2));
         } else {
-          $location.path($state.href('app').slice(2));
+          if ($stateParams.next) {
+            $location.path($state.href($stateParams.next).slice(2));
+          } else {
+            $location.path($state.href('app').slice(2));
+          }
         }
+
       }, function (req) {
         $scope.signup.errors = Error.signInFb(req.data);
       });
