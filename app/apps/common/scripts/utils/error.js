8aafc6b5fa4240cd5b23b36917effe6bc79f9311
Gerald Pharin <gerald@pharin.me>
2015-08-18 16:12:13 +0000

    fix(auth): display email error of fb in email section only

--- app/apps/common/scripts/utils/error.js
+++ app/apps/common/scripts/utils/error.js
@@ -83,7 +83,6 @@ angular
         } else if (data.nonFieldErrors[0] === '22') {
           // User account not activated yet.
           errors.email = ErrorConstants.fbAccountExists;
-          errors.fbTop = ErrorConstants.fbAccountExists;
         }
       }
 
