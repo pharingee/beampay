771669cb308edf12bff341fc3f849eff28111954
Gerald Pharin <gerald@pharin.me>
2015-08-18 16:11:00 +0000

    fix(auth): update signin with email with fb account error

--- app/apps/common/scripts/constants/errors.js
+++ app/apps/common/scripts/constants/errors.js
@@ -6,31 +6,47 @@ angular
   .constant('ErrorConstants', {
     unknownError: 'Oops! Something seems to have gone wrong.',
     unsupportedCountry: 'Your country is not supported. We\'ll let you know when we support it',
+
+    //signup errors
     weakPassword: 'The password must be at least 8 characters long and contain at least one numeric digit.',
     emailInUse: 'An account with this email has already been created. Please login if you are the owner of this email. Else enter another email.',
     unactivatedEmail: 'An activation link has been sent to your email. Please check your email for the next steps.',
     unmatchingPasswords: 'The two passwords do not match',
     privacyUnaccepted: 'Plese accept the Privacy Policy to continue',
+
+    //signin errors
     disabledAccount: 'Sorry, your account has been disabled',
     invalidLoginCredentials: 'Your email or password is incorrect. Please check and try again',
     emailOrPassEmpty: 'Please enter both email and password',
     adimnLogin: 'Admin accounts not allowed',
+    fbAccountExists: 'This email address belongs to a Facebook user registered on Beam. Please sign in with Facebook.',
+
+    //fbsignup errors
     fbUnknownError: 'Oops! Something seems to have gone wrong with your signup. No worries, you can still signup using your Email',
     unverifiedFbAccount: 'Oops! Your facebook account is not confirmed. Please confirm your facebook account and try again. Or sign up using your email.',
     fbPermissionRejected: 'We could not retrieve your email address. Please use sign up with email or allow us to acces your email via Facebook.',
+
+    //activation errors
     invalidActivationKey: 'The activation link you used was invalid. Please check and try again',
     expiredActivationKey: 'The activation link you used has expired. Please try signing up again',
+
+    //resendActivation errors
     unrecognizedEmail: 'Sorry, this email is not recognized. Are you trying to signup? Please click on the signup link on the header.',
     activatedEmail: 'You account has already been activated. Please try loggin in instead',
     invalidEmail: 'Please enter a valid email',
+
+    //change email
     emailExists: 'An account with this email has already been created. Please login if you are the owner of this email. Else enter another email.',
     unchangedEmail: 'Your email has not been changed',
+
+    //change password
     incorrectPassword: 'Sorry, your old Password is incorrect',
     unsetPassword: 'Passwords not set',
+
+    //transaction errors
     invalidParameters: 'Some of the information you provided is incorrect. Please check and try again.',
     exchangeRateChanged: 'The international exchange rate has changed. Please choose your service again.',
     incompleteProfile: 'Your profile is incomplete please update your details in your settings',
     invalidReferral: 'You have entered an invalid referral code. Please try again',
-    setPassword: 'A password has already been set. If you cannot remember please logout and send a reset password to your email.',
-    fbAccountExists: 'A Facebook account exists with this email. Please sign in with Facebook if this email belongs to you'
+    setPassword: 'A password has already been set. If you cannot remember please logout and send a reset password to your email.'
   });
